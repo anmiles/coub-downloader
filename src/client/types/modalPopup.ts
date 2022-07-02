a0ba97a185ffecadc7d7cbf348d9c459e8183922
Anatoliy Oblaukhov <mail@anmiles.net>
2022-07-02 11:02:13 +0300

    Fix linting

--- src/client/types/modalPopup.ts
+++ src/client/types/modalPopup.ts
@@ -1,5 +1,5 @@
 export type ModalPopup = {
-	show: ({content, classes}: {content: string, classes: string}) => ModalPopup;
+	show: ({ content, classes }: {content: string, classes: string}) => ModalPopup;
 	setContent: (content: string) => ModalPopup;
 	popup: {
 		close: () => void;
